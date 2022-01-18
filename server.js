ef0c734108636eabd0a34a95b3f6b98646fe993a
Patrick Villanueva <pjvillan@deakin.edu.au>
2022-01-18 14:03:16 +1100

    Add dependency
    - Added swagger-ui-express

--- server.js
+++ server.js
@@ -1,55 +1,60 @@
-require('dotenv').config();
-const mongoose = require('mongoose')
-const express = require('express');
-const path = require('path');
-const connectDB = require('./dbConnect');
-const routes = require('./routes')
-var expressLayouts = require('express-ejs-layouts');
-var flash = require('connect-flash');
-var session = require('express-session');
+require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
+const path = require("path");
+const connectDB = require("./dbConnect");
+const routes = require("./routes");
+var expressLayouts = require("express-ejs-layouts");
+var flash = require("connect-flash");
+var session = require("express-session");
+const swaggerUi = require(`swagger-ui-express`);
+const swaggerDocument = require(`./swagger.json`);
 
 const app = express();
 // EJS added as view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(expressLayouts);
-app.use(session({
-    secret:'cat',
-    resave:true,
-    saveUninitialized:true
-  }))
+app.use(
+    session({
+        secret: "cat",
+        resave: true,
+        saveUninitialized: true,
+    })
+);
 // const cros = require('cors');
-var cors = require('cors')
+var cors = require("cors");
 
 app.use(cors());
 
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname + "/public"));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 // app.use(cros);
 
 // connect flash
 app.use(flash());
 
 // Global Vars
-app.use((req,res,next)=>{
-    res.locals.success_msg = req.flash('success_msg')
-    res.locals.error_msg = req.flash('error_msg')
-    res.locals.error = req.flash('error')
-    next()
-  })
-  
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash("success_msg");
+    res.locals.error_msg = req.flash("error_msg");
+    res.locals.error = req.flash("error");
+    next();
+});
+
 app.use("/api", routes.projectService);
 app.use("/aws/obj", routes.s3Object);
 app.use("/aws/env", routes.s3Environment);
 app.use("/objects", routes.dbObjects);
 app.use("/environments", routes.dbEnvironments);
 
-app.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname,'public','404.html'));
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, "public", "404.html"));
 });
 
 connectDB();
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 const PORT = process.env.PORT || 3001;
-app.listen(PORT,()=>{  
-    console.log(`Server listening on ${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
 });
