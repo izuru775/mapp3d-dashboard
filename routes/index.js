283ae8130cf7d66012de5bbb946f27944516fa55
Patrick Villanueva <pjvillan@deakin.edu.au>
2022-01-18 12:37:03 +1100

    Fix imports
    - Fixed wrong imports due to the directory changes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 module.exports = {
     projectService: require("./projects"),
-    s3Object: require(`./object`),
-    s3Environment: require(`./environment`),
+    s3Object: require(`./aws/object`),
+    s3Environment: require(`./aws/environment`),
     dbObjects: require(`./mongoDB/objects`),
     dbEnvironments: require(`./mongoDB/environments`),
 };
