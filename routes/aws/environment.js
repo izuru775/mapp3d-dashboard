f9ce85004fe49b3c90aaccad530a595620450b2e
Patrick Villanueva <pjvillan@deakin.edu.au>
2022-01-19 16:26:51 +1100

    Update S3 routes
    - Updated the routes for the S3 objects and environments to make use of the new controllers
    - Created a new route to use the new function to check if a specified file exists in the bucket

--- routes/aws/environment.js
+++ routes/aws/environment.js
@@ -1,65 +1,11 @@
-require(`dotenv`).config();
 const express = require(`express`);
 const router = express.Router();
-const multer = require(`multer`);
-const multerS3 = require(`multer-s3`);
-const AWS = require(`aws-sdk`);
-const path = require("path");
+const Controllers = require('../../controllers');
 
-AWS.config.update({
-    region: process.env.REGION,
-});
-
-const s3 = new AWS.S3();
-
-const BUCKET = process.env.BUCKET;
-
-const upload = multer({
-    storage: multerS3({
-        s3: s3,
-        bucket: BUCKET,
-        acl: `public-read`,
-        key: (req, file, cb) => {
-            cb(null, `Environments/${Date.now()}-${file.originalname}`);
-        },
-    }),
-});
-
-// AWS Routes
-router.get(`/list`, async (req, res) => {
-    let response = await s3.listObjectsV2({ Bucket: BUCKET }).promise();
-    let key = response.Contents.map((item) => item.Key);
-    res.json({
-        message: `Success`,
-        data: key,
-        statusCode: 200,
-    }).status(200);
-});
-
-router.post(`/upload`, upload.single(`file`), async (req, res, next) => {
-    console.log(req.file);
-    let location = req.file.location;
-    res.json({
-        message: `Successfully uploaded file!`,
-        data: location,
-        statusCode: 200,
-    }).status(200);
-});
-
-router.get(`/download/:filename`, async (req, res) => {
-    const filename = req.params.filename;
-    let file = await s3.getObject({ Bucket: BUCKET, Key: `Environments/${filename}` }).promise();
-    res.send(file.Body);
-});
-
-router.delete(`/delete/:filename`, async (req, res) => {
-    const filename = req.params.filename;
-    await s3.deleteObject({ Bucket: BUCKET, Key: `Environments/${filename}` }).promise();
-    res.json({
-        message: `Successfully deleted file!`,
-        file: filename,
-        statusCode: 200,
-    }).status(200);
-});
+router.get(`/list`, Controllers.S3Environment.getEnvironments);
+router.post(`/upload`, Controllers.S3Environment.upload.single(`file`), Controllers.S3Environment.uploadEnvironment);
+router.get(`/download/:filename`, Controllers.S3Environment.downloadEnvironment);
+router.delete(`/delete/:filename`, Controllers.S3Environment.deleteEnvironment);
+router.get(`/exist/:filename`, Controllers.S3Environment.doesExist);
 
 module.exports = router;
--- routes/aws/object.js
+++ routes/aws/object.js
@@ -1,65 +1,11 @@
-require(`dotenv`).config();
 const express = require(`express`);
 const router = express.Router();
-const multer = require(`multer`);
-const multerS3 = require(`multer-s3`);
-const AWS = require(`aws-sdk`);
-const path = require("path");
+const Controllers = require('../../controllers');
 
-AWS.config.update({
-    region: process.env.REGION,
-});
-
-const s3 = new AWS.S3();
-
-const BUCKET = process.env.BUCKET;
-
-const upload = multer({
-    storage: multerS3({
-        s3: s3,
-        bucket: BUCKET,
-        acl: `public-read`,
-        key: (req, file, cb) => {
-            cb(null, `Objects/${Date.now()}-${file.originalname}`);
-        },
-    }),
-});
-
-// AWS Routes
-router.get(`/list`, async (req, res) => {
-    let response = await s3.listObjectsV2({ Bucket: BUCKET }).promise();
-    let key = response.Contents.map((item) => item.Key);
-    res.json({
-        message: `Success`,
-        data: key,
-        statusCode: 200,
-    }).status(200);
-});
-
-router.post(`/upload`, upload.single(`file`), async (req, res, next) => {
-    console.log(req.file);
-    let location = req.file.location;
-    res.json({
-        message: `Successfully uploaded file!`,
-        data: location,
-        statusCode: 200,
-    }).status(200);
-});
-
-router.get(`/download/:filename`, async (req, res) => {
-    const filename = req.params.filename;
-    let file = await s3.getObject({ Bucket: BUCKET, Key: `Objects/${filename}` }).promise();
-    res.send(file.Body);
-});
-
-router.delete(`/delete/:filename`, async (req, res) => {
-    const filename = req.params.filename;
-    await s3.deleteObject({ Bucket: BUCKET, Key: `Objects/${filename}` }).promise();
-    res.json({
-        message: `Successfully deleted file!`,
-        file: filename,
-        statusCode: 200,
-    }).status(200);
-});
+router.get(`/list`, Controllers.S3Object.getObjects);
+router.post(`/upload`, Controllers.S3Object.upload.single(`file`), Controllers.S3Object.uploadObject);
+router.get(`/download/:filename`, Controllers.S3Object.downloadObject);
+router.delete(`/delete/:filename`, Controllers.S3Object.deleteObject);
+router.get(`/exist/:filename`, Controllers.S3Object.doesExist);
 
 module.exports = router;
